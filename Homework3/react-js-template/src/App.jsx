35fecdc74f770bdbaf8ab53d18cf38332c5643bd
jz_2003 <zhaojianxing2003@gmail.com>
2026-05-11 22:28:12 -0700

    Homework3

--- Homework3/react-js-template/src/App.jsx
+++ Homework3/react-js-template/src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import RenderOptions from "./component/options";
-import { BarChart } from "./component/example";
+import LineChart from "./component/LineChart";
+import NewsList from "./component/NewsList";
+import TSNEScatter from "./component/TSNEScatter";
+
 function App() {
+  const [selectedStock, setSelectedStock] = useState("AAPL");
+
   return (
     <div className="flex flex-col h-full w-full">
       <header className="bg-zinc-400 text-white p-2 flex flex-row align-center">
         <h2 className="text-left text-2xl">Homework 3</h2>
         <label htmlFor="bar-select" className="mx-2">
-          Select a category:
+          Select a stock:
           <select
             id="bar-select"
             className="bg-white text-black p-2 rounded mx-2"
+            value={selectedStock}
+            onChange={(event) => setSelectedStock(event.target.value)}
           >
             <RenderOptions />
           </select>
@@ -18,28 +26,28 @@ function App() {
       <div className="flex flex-row h-full w-full">
         <div className="flex flex-col w-2/3">
           <div className="h-1/4 p-2">
-            <h3 className="text-left text-xl">
-              View 1 to be replaced by the view title
+            <h3 className="text-left text-xl h-8">
+              {selectedStock} Open, High, Low, and Close
             </h3>
-            <div className="border-2 border-gray-300 rounded-xl">
-              <BarChart />
+            <div className="border-2 border-gray-300 rounded-xl h-[calc(100%_-_2rem)]">
+              <LineChart selectedStock={selectedStock} />
             </div>
           </div>
           <div className="h-3/4 p-2">
             <h3 className="text-left text-xl h-8">
-              View 2 to be replaced by the view title
+              t-SNE Stock Map by Sector
             </h3>
             <div className="border-2 border-gray-300 rounded-xl h-[calc(100%_-_2rem)]">
-              <p className="text-center text-gray-500 mt-20">Empty View 2</p>
+              <TSNEScatter selectedStock={selectedStock} />
             </div>
           </div>
         </div>
         <div className="w-1/3 h-full p-2">
           <h3 className="text-left text-xl h-8">
-            View 3 to be replaced by the view title
+            {selectedStock} News
           </h3>
           <div className="border-2 border-gray-300 rounded-xl h-[calc(100%-2rem)]">
-            <p className="text-center text-gray-500 mt-20">Empty View 3</p>
+            <NewsList selectedStock={selectedStock} />
           </div>
         </div>
       </div>
--- Homework3/react-js-template/src/component/options.jsx
+++ Homework3/react-js-template/src/component/options.jsx
@@ -1,10 +1,13 @@
-import Data from "../../data/demo.json";
+const stockFiles = import.meta.glob("../../data/stockdata/*.csv");
 
 export default function RenderOptions() {
-    const bars = Data.data;
-    return bars.map((bar, index) => (
-      <option key={index} value={bar.category}>
-        {bar.category}
+    const tickers = Object.keys(stockFiles)
+      .map((path) => path.split("/").pop().replace(".csv", ""))
+      .sort();
+
+    return tickers.map((ticker) => (
+      <option key={ticker} value={ticker}>
+        {ticker}
       </option>
     ));
   }
